ae11f1ac9dc1990bab18805ead4a2c29d7c3bdeb
Yoryidan Angulo <angulotorresyoryidan@gmail.com>
2025-04-04 17:51:14 -0500

    feat: Primeras 3 preguntas.

--- Preguntas_y_Resppuestas_de_pregunta_general.py.py
+++ Preguntas_y_Resppuestas_de_pregunta_general.py.py
@@ -1,1 +1,17 @@
-prueba
+#primeras tres preguntas Santiago De Leon
+preguntas = [
+    {
+        "pregunta": "¿Cuál es la capital de brasil?",
+        "opciones": ["Rio de Janeiro", "Sao paulo", "Brasilia", "Manaos"],
+        "respuesta_correcta": 3
+    },
+    {
+        "pregunta": "¿Cuánto es 5 x 6?",
+        "opciones": ["16", "25", "11", "30"],
+        "respuesta_correcta": 4
+    },
+    {
+        "pregunta": "¿Quien descubrio America?",
+        "opciones": ["Simon Bolivar", "Cristobal Colon", "La Policarpa", "Juan Manuel Santos"],
+        "respuesta_correcta": 4
+    },
