8a86d4c8c8cf7c8395243d9ab415aca7ff1962ba
forlan Ordoñez <nalrofor@gmail.com>
2025-04-04 17:58:17 -0500

    se agregaron las 3 preguntas faltantes

--- Preguntas_y_Resppuestas_de_pregunta_general.py.py
+++ Preguntas_y_Resppuestas_de_pregunta_general.py.py
@@ -15,3 +15,22 @@ preguntas = [
         "opciones": ["Simon Bolivar", "Cristobal Colon", "La Policarpa", "Juan Manuel Santos"],
         "respuesta_correcta": 4
     },
+    
+    # agregadas las ultimas 3 preguntas
+    # Ingeniero Forlan
+        {
+        "pregunta": "¿Quien fue el responsable de la muerte de Pablo Escobar?",
+        "opciones": ["La Guerrilla de las Farc", "El cartel de Cali", "La DEA", "El bloque de busqueda"],
+        "respuesta_correcta": 4
+    },
+    {
+        "pregunta": "¿De dónde es el sombrero vueltiao?",
+        "opciones": ["La Guajira", "Magdalena", "Sucre", "Cordoba"],
+        "respuesta_correcta": 4
+    },
+     {
+        "pregunta": "¿Cuál es el río más largo que pasa por Colombia'?",
+        "opciones": ["Cauca", "Sinu", "Amazonas", "Magdalena"],
+        "respuesta_correcta": 3
+    }
+]
